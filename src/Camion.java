56284e5177cd32635551b1ce3e17634e088016a1
plopezh <plopezh96@gmail.com>
2017-03-24 09:54:45 +0100

    Update Camion.java

--- src/Camion.java
+++ src/Camion.java
@@ -5,18 +5,17 @@ public class Camion {
 	private String idUsine;
 	private double capacite;
 	private double vitesse;
-	private double coutTransport;
+	private double coutTransportkm;
 	private double heuresJour;
 	private double capaciteUtilisee;
 	private double heuresUtilisees;
 	
 	
-	public Camion(String id, String usine, double capacite, double vitesse, double coutTransport, double heuresJour, double capaciteUtilisee, double heuresUtilisees){
-		this.id=id;
+	public Camion(String id, String usine, double capacite, double vitesse, double coutTransport, double heuresJour){
 		this.idUsine=usine;
 		this.capacite=capacite;
 		this.vitesse=vitesse;
-		this.coutTransport=coutTransport;
+		this.coutTransportkm=coutTransport;
 		this.heuresJour=heuresJour;
 		this.capaciteUtilisee=0;
 		this.heuresUtilisees=0;
