c6bacc4fbb6f21cbd01560a16e65a2a6f33c1753
plopezh <plopezh96@gmail.com>
2017-03-27 16:00:43 +0200

    Update map.java

--- src/map.java
+++ src/map.java
@@ -1,15 +1,66 @@
-public Client lePlusProche(Camion camion, List<Client> clients){
-		int i=0;
-		int j=0;
-		Iterator<Client> iter = clients.iterator() ;
-		double distance= camion.distance(iter.getFirst().getcoord());
-		while (iter.hasNext()) {
-		Client c = iter.next() ;
-			if (distance < camion.getcoord().distance(iter.getFirst().getcoord())){
-				distance=camion.distance(iter.getFirst.getcoord());
-				j=i;
-			}
-			iter.removeFirst();
-			i++;
-		}
-		return clients.get(j);
+ public static void main(String[] args) {
+        	
+        	
+        	InputFile f = new InputFile();
+        	f.open("Exemple.txt");
+        	
+        		
+        	int nbjours= f.readInt();
+        	int nbusines=f.readInt();
+        	int nbclients= f.readInt();
+        	int nbcamions= f.readInt();
+        		
+        		
+        	
+        	double[][][] usine= new double [nbusines][4][nbjours];
+        	double[][] coordusines= new double [nbusines][2];
+        	double[][] clients= new double[nbclients][6];
+        	double[][] camions= new double [nbcamions][10];
+        	double[][] prestataire= new double[2][7];  		
+        	String [] usinescamions= new String[nbcamions];
+        		
+        	
+        	
+        	for(int i=0; i<nbusines; i++ ){
+        	String idUsine= f.readString();
+        	coordusines[i][0]= f.readDouble();
+        	coordusines[i][1]= f.readDouble();
+        	for( int j =0; j<4;j++){
+        		for(int k=0; k<nbjours; k++){
+        			usine[i][j][k]= f.readDouble();
+        			
+        			
+        		}
+        	}
+        	}
+        	
+        	for(int i=0; i<nbclients; i++){
+        		String idClient= f.readString();
+        		for (int j=0; j<6;j++){
+        			clients[i][j]=f.readDouble();
+        		}
+        	}
+        	
+        	for( int i=0; i<nbcamions; i++){
+        		String idCamion=f.readString();
+        		usinescamions[i]=f.readString();
+        		for(int j=0; j<10; j++){
+        			camions[i][j]= f.readDouble();
+        			}
+        		}
+        	
+        	for (int i=0; i<2;i++){
+        		for(int j=0; j<7;j++){
+        			prestataire[i][j]=f.readDouble();
+        		}
+        	
+        	}
+        	
+        	
+        	
+        	
+        	
+        	f.close();
+        	
+        	
+        }
