66d6e62d9ac9d4a1ef6f720c3ef4edac14cbc66d
plopezh <plopezh96@gmail.com>
2017-03-27 14:58:02 +0200

    Update map.java

--- src/map.java
+++ src/map.java
@@ -1,4 +1,15 @@
-
-public class map {
-
-}
+public Client lePlusProche(Camion camion, List<Client> clients){
+		int i=0;
+		int j=0;
+		Iterator<Client> iter = clients.iterator() ;
+		double distance= camion.distance(iter.getFirst().getcoord());
+		while (iter.hasNext()) {
+		Client c = iter.next() ;
+			if (distance < camion.getcoord().distance(iter.getFirst().getcoord())){
+				distance=camion.distance(iter.getFirst.getcoord());
+				j=i;
+			}
+			iter.removeFirst();
+			i++;
+		}
+		return clients.get(j);
