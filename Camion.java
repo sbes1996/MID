c815198437c477c06a64d344aecbcba2440ab0a1
plopezh <plopezh96@gmail.com>
2017-03-28 09:46:22 +0200

    Update Camion.java

--- Camion.java
+++ Camion.java
@@ -1,112 +1,119 @@
+import java.util.LinkedList;
 
-public class Camion {
+public class Usine {
+
+	
 	private String id;
-	private String idUsine;
-	private double capacite;
-	private double vitesse;
-	private double coutTransportkm;
-	private double[] heuresJour;
-	private double capaciteUtilisee;
-	private double heuresUtilisees;
-	private double cout;
-	private Usine usine; // pas sur
-	private Point coord; // pas sur
+	private double[] capaciteProd;
+	private double[] capaciteStock;
+	private double[] coutProd;
+	private double[] coutStock;
+	private double[] stock;
+	private double[] production;
+	private Point coord;
+	private LinkedList<Client> clients;
+	private LinkedList<Camion> flotteCamion;
+	private double[] quantiteALivrer;
 	
-	public Camion(String id, String idusine, double capacite, double vitesse, double coutTransport, double[] heuresJour, Usine usine){
-		this.idUsine=idusine;
-		this.capacite=capacite;
-		this.vitesse=vitesse;
-		this.coutTransportkm=coutTransport;
-		this.capaciteUtilisee=0;
-		this.heuresUtilisees=0;
-		this.cout=0;
-		this.usine=usine;
-		this.coord= this.usine.getCoord();
-		this.heuresJour= new double[7];
-		this.heuresJour=heuresJour;
-		for( int i=0; i<7; i++){
-			this.heuresJour[i]=heuresJour[i];
+	
+	
+	public Usine(String id,double[] capaProd, double[] capaStock, double[] coutStock, double[] coutProd, Point coord, LinkedList<Client> clients){
+		this.id=id;
+		this.coord=coord;
+		this.capaciteProd=new double[7];
+		this.capaciteStock=new double[7];
+		this.coutStock=new double[7];
+		this.coutProd=new double[7];
+		this.stock= new double[7];
+		this.production=new double[7];
+		for (int i=0; i<7; i++){
+			this.capaciteProd[i]=capaProd[i];
+			this.capaciteStock[i]=capaStock[i];
+			this.coutStock[i]= coutStock[i];
+			this.coutProd[i]= coutProd[i];
+			this.production[i]=0;
+			this.stock[i]=0;
 		}
-	}
-	
-	
-	
-	public void addHeures( double heures){
-		//if(this.heuresUtilisees + heures <= this.heuresJour){
-			this.heuresUtilisees= this.heuresUtilisees + heures;
 		
-	}
-	
-
-	public void addCapacite( double capa){
-		if( this.capaciteUtilisee + capa< this.capacite){
-			this.capaciteUtilisee= this.capaciteUtilisee + capa;
-		} elsif( this.capaciteUtilisee+capa>= this.capacite){
-			this.capaciteUtilisee=this.capacite;
+		this.flotteCamion=new LinkedList<Camion>() ;
+		this.clients=clients;
+		this.quantiteALivrer= new double[7];
+		for( int i=0; i<6; i++){
+			this.quantiteALivrer[i]= this.production[i]+this.stock[i]-this.stock[i+1];
 		}
+		this.quantiteALivrer[6]= this.production[6]+this.stock[6];
+	}
+		
+	
+	public LinkedList<Client> getClients(){
+		return this.clients;
 	}
 	
-	
-	public double getCapaUtilisee(){
-		return this.capaciteUtilisee;
+	public LinkedList<Camion> getCamions(){
+		return this.flotteCamion;
 	}
 	
-	public void setCoord( Point p){
-		this.coord= p;
+	public double getQuantiteALivrer( int j){
+		return this.quantiteALivrer[j];
 	}
+
+	
+		
 	public Point getCoord(){
 		return this.coord;
 	}
 	
-	public void setCout( double cout) {
-		this.cout= this.cout + cout;
-	}
-	
-	public double getTempsTraject(Point depart, Point arrivee){
-		return( depart.distance(arrivee)/vitesse);
-	}
-	
-	
-	public double getCoutTraject( Point depart, Point arrivee){
-		return getTempsTraject(depart, arrivee)*this.coutTransportkm;
-	}
-	
-	
-	
-	public void jourSuivant(){
-		this.cout=0;
-		this.heuresUtilisees=0;
-	}
-	
-	
-	public void delivre(Client c, double quantite,int j){
-		this.cout= this.cout + getCoutTraject(this.coord, c.getCoordonnees());
-		setCoord(c.getCoordonnees());
-		this.capaciteUtilisee=this.capaciteUtilisee-quantite;
-		c.setMarchandise(j,quantite);
+	public void setProd(int prod, int jour){
+		if( this.capaciteProd[jour]>= prod)
+		this.production[jour]= prod;
 		
 	}
 	
+	public void setStock(int stock, int jour){
+		if( this.capaciteStock[jour]>= stock)
+		this.production[jour]= stock;
+		
+	}
 	
+	public double getCapaciteProd(int j){
+		return this.capaciteProd[j];
+	}
 	
-	public boolean peutRetourner(Client c, int jour){
-		if (getTempsTraject(this.coord, c.getCoordonnees()) + getTempsTraject(c.getCoordonnees(), this.usine.getCoord()) + this.heuresUtilisees   < this.heuresJour[jour]){
-			return true;
+	 public double getCapaciteProdTotale(){
+		 double capa=0;
+		 for(int i=0; i<7; i++){
+			 capa=capa+ getCapaciteProd(i);
+		 }
+		 return capa;
+	 }
+	
+	public double getCapaciteStock(int j){
+		return this.capaciteStock[j];
+	}
+		
+	public double getCapaciteStockTotale(){
+		double capa=0;
+		for( int i =0; i<7; i++){
+			capa= capa+ getCapaciteStock(i);
 		}
-		return false;
-		
+		return capa;
 	}
-	// on veut savoir si une fois s'etre déplacé au point p on aura le temps de revenir
-	public void retourUsine() {
-		this.coord = this.usine.getCoord();
-		this.cout = this.cout + getCoutTraject(this.coord, this.usine.getCoord());
-		this.heuresUtilisees=this.heuresUtilisees+getCoutTraject(this.coord, this.usine.getCoord());
-		 
 		
+	
+	public void addClient( Client c){
+		this.clients.add(c);
 	}
 	
+	public void addCamion( Camion c){
+		this.flotteCamion.add(c);
+	}
 	
+	public double getStock (int j){
+		return this.stock[j];
+	}
 	
+	public double getProd(int j){
+		return this.production[j];
 	
-
+}
 }
