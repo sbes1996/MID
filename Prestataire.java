04573412886c4b84cd1de99e8843cc39f682713f
sbes1996 <simon.besnier1996@gmail.com>
2017-03-28 13:57:38 +0200

    Update Prestataire.java

--- Prestataire.java
+++ Prestataire.java
@@ -1,19 +1,21 @@
 public class Prestataire {
 
-	private double coutFixe;
-	private double coutTransport;
+	private double[] coutFixe;
+	private double[] coutTransport;
 	private double distanceUtilise;
 	private double distance;
 	private double couts;
+	private int dureeExp;
 	
 	
 	
 	
-	public Prestataire(double coutFixe,double coutTransport){
+	public Prestataire(double[] coutFixe,double[] coutTransport, int duree){
 		this.coutFixe=coutFixe;
 		this.coutTransport=coutTransport;
 		this.distanceUtilise=0;
 		this.couts=0;
+		this.dureeExp=duree;
 		
 	}
 	
@@ -24,9 +26,9 @@ public class Prestataire {
 	public void addCout(double cout){
 		this.couts=this.couts + cout;
 	}
-	public double getCoutFinal(){
+	public double getCoutFinal(int j){
 		if (this.couts!=0){
-			this.couts=this.couts+this.coutFixe;
+			this.couts=this.couts+this.coutFixe[j];
 		}
 		return this.couts;
 	}
@@ -41,7 +43,7 @@ public class Prestataire {
 
 	public void prestDelivre(Client client, Usine u,double quantite,int j){
 		this.addDistance(u.getCoord(), client.getCoord());
-		double cout=coutTransport*u.getCoord().distance(client.getCoord());
+		double cout=coutTransport[j]*u.getCoord().distance(client.getCoord());
 		this.addCout(cout);
 		client.setMarchandise(j, quantite);
 		
@@ -55,5 +57,3 @@ public class Prestataire {
 	
 }
 
-
-	
